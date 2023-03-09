a8e2bb1df2a595cd4e5d81e3d8bfe43c67004e5b
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-03-09 17:36:05 +0530

    leetcode challenge

---
+++ .vscode/leetcode-daily-challenge/875. Koko Eating Bananas.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+private:
+    int check(int perhr, vector<int> &piles, int h)
+    {
+        int count = 0;
+        for (int &it : piles)
+        {
+            count += it / perhr;
+            if (it % perhr)
+                count += 1;
+        }
+        return count <= h;
+    }
+
+public:
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int l = 1, r = 1000000000;
+        while (l < r)
+        {
+            int m = (l + r) / 2, total = 0;
+            for (int p : piles)
+                total += (p + m - 1) / m;
+            if (total > h)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
+    }
+};
