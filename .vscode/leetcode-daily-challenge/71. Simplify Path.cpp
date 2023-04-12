688af5718c9814fd604f1359ef47f8c0a810dd50
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-04-13 02:05:27 +0530

    added today challenge

---
+++ .vscode/leetcode-daily-challenge/71. Simplify Path.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    string simplifyPath(string path)
+    {
+        string ans;
+        stack<string> st;
+
+        for (int i = 0; i < path.size(); i++)
+        {
+            if (path[i] == '/')
+                continue;
+            string temp;
+            while (i < path.size() && path[i] != '/')
+            {
+                temp += path[i];
+                i++;
+            }
+            if (temp == ".")
+                continue;
+            else if (temp == "..")
+            {
+                if (!st.empty())
+                {
+                    st.pop();
+                }
+            }
+            else
+            {
+                st.push(temp);
+            }
+        }
+        while (!st.empty())
+        {
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+        if (ans.size() == 0)
+            return "/";
+        return ans;
+    }
+};
