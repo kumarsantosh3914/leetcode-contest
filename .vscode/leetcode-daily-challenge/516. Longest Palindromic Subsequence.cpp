9203c5f287527451baffd0221efa833aceeb598a
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-04-15 01:19:50 +0530

    added today challenge

---
+++ .vscode/leetcode-daily-challenge/516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+private:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // return helper(n-1, m-1, text1, text2, dp);
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+
+public:
+    int longestPalindromeSubseq(string s)
+    {
+        string t = s;
+        reverse(t.begin(), t.end());
+        return longestCommonSubsequence(s, t);
+    }
+};
