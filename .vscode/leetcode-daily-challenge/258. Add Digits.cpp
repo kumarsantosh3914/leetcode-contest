c38d451ce7561c1c22d13780c702e3455df39ef2
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-04-27 15:59:11 +0530

    added today challenge

---
+++ .vscode/leetcode-daily-challenge/258. Add Digits.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        int sum = 0;
+        while (num)
+        {
+            sum += (num % 10);
+            num /= 10;
+        }
+        if (sum < 10)
+            return sum;
+        return addDigits(sum);
+    }
+};
