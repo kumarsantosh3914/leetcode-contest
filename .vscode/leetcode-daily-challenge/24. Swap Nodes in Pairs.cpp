9746b498be3961a5187535b0a7ecba2aeb78662e
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-17 01:42:34 +0530

    added today challenge

---
+++ .vscode/leetcode-daily-challenge/24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        ListNode *dummyNode = new ListNode();
+        ListNode *prevNode = dummyNode;
+        ListNode *currNode = head;
+
+        if (!head || !head->next)
+            return head;
+
+        while (currNode && currNode->next)
+        {
+            prevNode->next = currNode->next;
+            currNode->next = prevNode->next->next;
+            prevNode->next->next = currNode;
+
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+        return dummyNode->next;
+    }
+};
