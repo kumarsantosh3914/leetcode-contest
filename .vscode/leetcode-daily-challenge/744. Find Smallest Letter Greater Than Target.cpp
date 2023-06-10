79aabd4f5a422d18dee9d583a373e2ab65a92837
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-06-10 15:52:55 +0530

    added today challenge

---
+++ .vscode/leetcode-daily-challenge/744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
+        for (int i = 0; i < letters.size(); i++)
+        {
+            if (letters[i] > target)
+                return letters[i];
+        }
+        return letters[0];
+    }
+};
