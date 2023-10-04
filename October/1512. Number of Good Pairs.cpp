0c4232a51a89344122a6f59403607e81c276c715
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-04 23:12:17 +0530

    day 3 done

---
+++ October/1512. Number of Good Pairs.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int numIdenticalPairs(vector<int> &nums)
+    {
+        int n = nums.size();
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
