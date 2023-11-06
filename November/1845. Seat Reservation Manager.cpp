fa10d56619b2505ca39abaa1d75d2e04c224c1c7
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-11-07 02:03:28 +0530

    day 6 done

---
+++ November/1845. Seat Reservation Manager.cpp
@@ -0,0 +1,40 @@
+class SeatManager
+{
+private:
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+public:
+    SeatManager(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            pq.push(i);
+        }
+    }
+
+    int reserve()
+    {
+        if (!pq.empty())
+        {
+            int reserved = pq.top();
+            pq.pop();
+            return reserved;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    void unreserve(int seatNumber)
+    {
+        pq.push(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
