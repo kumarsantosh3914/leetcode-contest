d4d54dfc5ee878c3a7c144af2161fb7945c2516c
Santosh Kumar <santoshkumar15novmth@gmail.com>
2024-03-07 17:40:32 +0530

    day 7 done

---
+++ leetcode-daily-challenge-2024/March/876. Middle of the Linked List.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+private:
+    ListNode *getMiddle(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        if (head->next->next == NULL)
+        {
+            return head->next;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            if (fast != NULL)
+            {
+                fast = fast->next;
+            }
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        // return getMiddle(head);
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
