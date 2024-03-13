dea25a35c40680483d8bac82d628a5cb473fd646
Santosh Kumar <santoshkumar15novmth@gmail.com>
2024-03-13 23:16:26 +0530

    day 13 done

---
+++ leetcode-daily-challenge-2024/March/2485. Find the Pivot Integer.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int pivotInteger(int n)
+    {
+        vector<int> arr;
+        for (int i = 1; i < n; i++)
+        {
+            arr.push_back(i);
+        }
+        int i = 0;
+        if(arr[i]==arr[n]+i-arr[i]
+           return i;
+        else return -1;
+    }
+};
